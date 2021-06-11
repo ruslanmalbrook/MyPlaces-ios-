dbb461310fb978d49550f7a6433e4a89cc1d4075
Ruslan Netesov <ruslanprogit@gmail.com>
2021-06-11 11:10:51 +0700

    App icon Launch Screen Row Height

--- MyPlaces/ViewController.swift
+++ MyPlaces/ViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    let rowHeight: CGFloat = 85
+    
     let restaurantNames = [
         "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
         "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
@@ -28,10 +30,16 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell =  tableView.dequeueReusableCell(withIdentifier: "cell")
         cell?.textLabel?.text = restaurantNames[indexPath.row]
         cell?.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+        cell?.imageView?.layer.cornerRadius = rowHeight / 2
+        cell?.imageView?.clipsToBounds = true
         
         return cell!
     }
+    
+    //MARK: - Table View Delegate
 
-
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return rowHeight
+    }
 }
 
